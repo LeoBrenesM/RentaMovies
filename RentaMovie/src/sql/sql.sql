dd4aec4948a7dd8163d4072c1a962868dd130229
lbren <lbren@192.168.0.104>
2017-11-16 20:07:52 -0600

    El sql trae los comandos para crear el usuario, darle todos los privilegios y como TODO dejo las tablas y sus respectivas columnas y eso...

--- RentaMovie/src/sql/sql.sql
+++ RentaMovie/src/sql/sql.sql
@@ -8,3 +8,8 @@
  * Created: Nov 16, 2017
  */
 
+--en el sql plus o sql command line escribe esto: 
+create user proyecto identified by 1234;
+--le da enter
+grant all privileges to proyecto;
+--le da enter de nuevo y listo, un nuevo usuario donde tener nuestras bases de datos hahaha
