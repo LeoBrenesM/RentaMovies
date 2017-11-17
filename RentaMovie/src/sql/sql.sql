b35f9dcf95373756d8c9551edcfd2574291f7fe6
lbren <lbren@192.168.0.104>
2017-11-17 16:10:40 -0600

    Falta continuar con lo de registrar

--- RentaMovie/src/sql/sql.sql
+++ RentaMovie/src/sql/sql.sql
@@ -1,19 +1,19 @@
 --en el sql plus o sql command line escribe esto: 
 --create user proyecto identified by 1234;
 --le da enter
---grant all privileges to proyecto;
+grant all privileges to proyecto;
 --le da enter de nuevo y listo, un nuevo usuario donde tener nuestras bases de datos hahaha
---create table Cliente(id_cliente number(4), fecha_Nacimiento date, email nvarchar2(30),
---                     direccion nvarchar2(150), num_telefono number(10), nombre_Cliente nvarchar2(30) );
+create table Cliente(id_cliente number(4), fecha_Nacimiento date, email nvarchar2(30),
+                     direccion nvarchar2(150), num_telefono number(10), nombre_Cliente nvarchar2(30) );
 
---alter table Cliente add constraint PKCliente primary key (id_cliente);
+alter table Cliente add constraint PKCliente primary key (id_cliente);
 
 
-/*insert into Cliente values(1, TO_DATE('1998/10/14','yyyy/mm/dd'),'lbrenes10@hotmail.com',
- 'Costa Rica, San Jose, Goicoechea, Guadalupe...', 85572537, 'Leonardo Brenes' );
-*/
+insert into Cliente values(1, TO_DATE('1998/10/14','yyyy/mm/dd'),'lbrenes10@hotmail.com',
+                             'Costa Rica, San Jose, Goicoechea, Guadalupe...', 85572537, 'Leonardo Brenes' );
 
---select * from Cliente;
+
+select * from Cliente;
 
 
 create table Vendedor(id_vendedor number(4), password nvarchar2(20), fecha_Nacimiento date,
@@ -24,3 +24,21 @@ insert into Vendedor values(1, '1234', TO_DATE('1998/10/14','yyyy/mm/dd'), 'lbre
                      'Leonardo', 85572537, 'Costa Rica, San Jose, Goicoechea, Guadalupe...'  );
 
 select * from Vendedor;
+
+
+--funcion para el login...
+create or replace function fnLog(username in nvarchar2, pass in nvarchar2)
+return number
+as
+  resultado number:=0;
+  contraU nvarchar2(20);
+begin
+  select password into contraU from vendedor where username = email;
+  if contraU = pass then
+    resultado :=1;
+  else resultado :=0;
+  end if;
+  return resultado;
+end;
+/
+--funcion o sp para registrar...
